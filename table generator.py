7064ec976ae3582640f0ffff4249ba0e54561a8b
Chaitnya7 <chaitnya.raut111@gmail.com>
2025-07-24 13:42:27 +0530

    table genrator
    it can generate table of any number

---
+++ table generator.py
@@ -0,0 +1,5 @@
+t = int(input("enter the number to generate a table : ")) 
+for k in range(1,11):
+    print(f"{ t } * { k } = ",int(t*k)) 
+# the f {} string value can update no matter is it in string 
+# we can use if else for 10 to make it align with other 
